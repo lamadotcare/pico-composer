b9af4761bcda1137edd781185030bf4afc2f0ca4
padthaitofuhot <padthaitofuhot@users.noreply.github.com>
2020-08-17 00:20:26 -0400

    update

--- config/config.php
+++ config/config.php
@@ -20,7 +20,7 @@
  * BASIC
  */
 $config['site_title'] = 'Lansing Area Mutual Aid';              // Site title
-// $config['base_url'] = '';                    // Override base URL (e.g. http://example.com)
+$config['base_url'] = 'https://lama.care';                    // Override base URL (e.g. http://example.com)
 // $config['rewrite_url'] = null;               // A boolean indicating forced URL rewriting
 
 /*
